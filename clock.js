42c71c139734083bf86329c36759c55ae195aa06
Vetal <vitalikzeleneckij3@gmail.com>
2024-05-18 21:38:26 +0300

    add clock for all pages

---
+++ clock.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const clock = document.querySelector(".clock");
+
+function setTime() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  clock.textContent = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }`;
+}
+
+setInterval(setTime, 100);
